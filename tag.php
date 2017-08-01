e280fb1f519b4be08405f2925d8bc32c7e8c33de
esteban20 <esteban@velosoft.mx>
2017-08-01 18:29:12 -0500

    tag uno terminado

--- tag.php
+++ tag.php
@@ -6,10 +6,28 @@ getAside();
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Tag <span> > </span><span>Resumen</span>
-      </h1>     
+    <section class="content-header ">
+      <h1 class="linea">
+        <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Tag <span class="mayor"> > </span> <span class="r">Resumen</span>
+      </h1>
+      <div class="form-group linea2">        
+        <select class="form-control">
+          <option>MONTO</option>r
+          <option>option 2</option>
+          <option>option 3</option>
+          <option>option 4</option>
+          <option>option 5</option>
+        </select>
+      </div> 
+      <div class="form-group linea2">        
+        <select class="form-control">
+          <option>FECHA</option>
+          <option>option 2</option>
+          <option>option 3</option>
+          <option>option 4</option>
+          <option>option 5</option>
+        </select>
+      </div>    
     </section>
     <!-- Main content -->
     <section class="content"> 
@@ -21,49 +39,41 @@ getAside();
           <!-- tablas -->
       <div class="row">
         <div class="col-xs-12">
-          <div class="box">            
+          <div class="box no">            
             <!-- /.box-header -->
             <div class="box-body table-responsive no-padding">
               <table id="example2" class="table table-hover">
-                <thead>
+                <thead class="separacion">
                   <tr>
-                    <th>Nombre</th>
-                    <th>Promedio de Recargas</th>
-                    <th>Acumulado de Recargas</th>
-                    <th>Acumulado de Litros Magna</th>
-                    <th>Acumulado de Pesos Magna</th>
-                    <th>Acumulado de Litros Premium</th>
-                    <th>Acumulado de Pesos Premium</th>
-                    <th>Acumulado de Litros Diesel</th>
-                    <th>Acumulado de Pesos Diesel</th>
+                    <th>ID</th>
+                    <th>TAG</th>
+                    <th>FECHA</th>
+                    <th>SALDO</th>                    
                   </tr>
                 </thead>
                 <tbody class="mytabs">
                 <?php for ($i=0; $i <10 ; $i++) { ?>
                    <tr>
-                  <th>Lorem ipsum Dolor sir S:A de CV</th>
-                  <th>300</th>
-                  <th>2,300</th>
-                  <th>25,000</th>
-                  <th>$345,000</th>
-                  <th>30,000</th>
-                  <th>$420,000</th>
-                  <th>$50,000</th>
-                  <th>$540,000</th>
-                </tr>
+                    <th> 
+                    <a href="#">2910384561V</a></th>
+                    <th>
+                    <a href="#">Lorem ipsum Dolor</a>
+                    </th>
+                    <th>
+                    <a href="#">30/05/2017</a>
+                    </th>                  
+                    <th>
+                    <a href="#">$540,000</a>
+                    </th>
+                  </tr>
                <?php } ?>
 
                <?php for ($x=0; $x <10 ; $x++) { ?>
                 <tr>
-                  <th>ipsum Dolor sir S:A de CV</th>
-                  <th>400</th>
-                  <th>5,300</th>
-                  <th>30,000</th>
-                  <th>$645,000</th>
-                  <th>20,000</th>
-                  <th>$400,000</th>
-                  <th>$80,000</th>
-                  <th>$740,000</th>
+                  <th><a href="#">2356874561</a></th>
+                  <th><a href="#">ipsum Dolor</a></th>
+                  <th><a href="#">20/09/2017</a></th>                  
+                  <th><a href="#">$340,000</a></th>
                 </tr>
                <?php } ?>
               
