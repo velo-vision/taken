4a7784a5249bf702353872554321f98c23c39c71
jorge <jorge.f@velosoft.mx>
2017-08-07 13:29:23 -0500

    subir cambios

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 include('funtions.php');
 getHeader();
-getAside(); 
+getAside();
 
 ?>
 
@@ -10,8 +10,8 @@ getAside();
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Resumen de Actividad        
-      </h1>     
+        <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Resumen de Actividad
+      </h1>
     </section>
     <!-- Main content -->
     <section class="content">
@@ -24,7 +24,7 @@ getAside();
             <img src="img/index/img-7.png" class="resumen">
               <h3>16 17</h3>
               <p>PRECIO POR LITRO MAGNA</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
@@ -35,7 +35,7 @@ getAside();
             <img src="img/index/img-7.png" class="resumen">
               <h3>20 - 21</h3>
               <p>PRECIO POR LITRO PREMIUM</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
@@ -46,7 +46,7 @@ getAside();
             <img src="img/index/img-7.png" class="resumen">
               <h3>18 - 19</h3>
               <p>PRECIO POR LITRO DIESEL</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
@@ -57,13 +57,13 @@ getAside();
             <img src="img/index/img-5.png" class="resumen">
               <h3>80</h3>
               <p>CUENTAS ACTIVAS</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
       </div>
       <!-- OTROS 4 -->
-      <div class="row">        
+      <div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
           <div class="small-box bg-aqua">
@@ -71,7 +71,7 @@ getAside();
             <img src="img/index/img-1.png" class="resumen">
               <h3>$3,200,000</h3>
               <p>SALDO REQUERIDO MENSUAL</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
@@ -82,7 +82,7 @@ getAside();
             <img src="img/index/img-11.png" class="resumen" style="opacity: .5;">
               <h3>$1,000,000</h3>
               <p>COSTO PROM. DE RECARGAS X USUARIO</p>
-            </div>                        
+            </div>
           </div>
         </div>
         <!-- ./col -->
@@ -100,19 +100,19 @@ getAside();
           </div>
           <div class="col-lg-6">
              <div>
-              
+
             </div>
           </div>
           </div>
-          <section class="col-lg-12 connectedSortable">        
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding"> 
+          <section class="col-lg-12 connectedSortable">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
               <h3 class="size-titulo">
                 <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Historial de Dispersiones
               </h3>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-lg-offset-2">
-              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">        
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">
                 <select class="form-control texto-res">
                   <option>FECHA</option>
                   <option>option 2</option>
@@ -121,7 +121,7 @@ getAside();
                   <option>option 5</option>
                 </select>
               </div>
-                <div class=" col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">        
+                <div class=" col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">
                   <select class="form-control texto-res">
                     <option>MONTO</option>r
                     <option>option 2</option>
@@ -130,12 +130,12 @@ getAside();
                     <option>option 5</option>
                   </select>
                 </div>
-              </div>        
+              </div>
           </div>
           <!-- tablas -->
       <div class="row">
         <div class="col-xs-12">
-          <div class="box">            
+          <div class="box">
             <!-- /.box-header -->
             <div class="box-body table-responsive no-padding">
               <table id="example12" class="table table-hover">
@@ -145,7 +145,7 @@ getAside();
                     <th>Nombre / Alias</th>
                     <th>Fecha de Traspaso</th>
                     <th>Monto de Traspaso</th>
-                    <th>Saldo Total del Tag</th>                    
+                    <th>Saldo Total del Tag</th>
                   </tr>
                 </thead>
                 <tbody class="mytabs">
@@ -155,10 +155,10 @@ getAside();
                   <th>Nemo en emouy</th>
                   <th>30/09/2017</th>
                   <th>25,000</th>
-                  <th>$25,000</th>                  
+                  <th>$25,000</th>
                 </tr>
                <?php } ?>
-                </tbody> 
+                </tbody>
               </table>
             </div>
             <!-- /.box-body -->
@@ -169,19 +169,19 @@ getAside();
           <div class="col-xs-4 col-md-4 col-lg-5"></div>
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-2">
               <a href="#"><p class="botonActividad">VER TODA LA ACTIVIDAD</p></a>
-            </div>  
+            </div>
           <div class="col-xs-4 col-md-4 col-lg-5"></div>
         </div>
       </div>
       <!--fin primera tabla -->
-       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding"> 
+       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding">
           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
             <h3 class="size-titulo">
               <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Historial de Recargas de Gasolina
             </h3>
           </div>
           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-lg-offset-2">
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">        
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">
               <select class="form-control texto-res">
                 <option>FECHA</option>
                 <option>option 2</option>
@@ -190,7 +190,7 @@ getAside();
                 <option>option 5</option>
               </select>
             </div>
-              <div class=" col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">        
+              <div class=" col-xs-6 col-sm-6 col-md-6 col-lg-6 form-group listas-fij">
                 <select class="form-control texto-res">
                   <option>MONTO</option>r
                   <option>option 2</option>
@@ -199,13 +199,13 @@ getAside();
                   <option>option 5</option>
                 </select>
               </div>
-            </div>        
+            </div>
         </div>
         <!-- fint tiulo -->
         <!-- tablas -->
       <div class="row">
         <div class="col-xs-12">
-          <div class="box">            
+          <div class="box">
             <!-- /.box-header -->
             <div class="box-body table-responsive no-padding">
               <table id="example12" class="table table-hover">
@@ -232,7 +232,7 @@ getAside();
                   <th>29103</th>
                 </tr>
                <?php } ?>
-                </tbody> 
+                </tbody>
               </table>
             </div>
             <!-- /.box-body -->
@@ -243,29 +243,29 @@ getAside();
           <div class="col-xs-4 col-md-4 col-lg-5"></div>
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-2">
               <a href="#"><p class="botonActividad">VER TODA LA ACTIVIDAD</p></a>
-            </div>  
+            </div>
           <div class="col-xs-4 col-md-4 col-lg-5"></div>
         </div>
       </div>
       <!--fin segunda tabla -->
       <!-- mapa   -->
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding"> 
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 separacion-tag sin-padding">
           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
             <h3 class="size-titulo">
               <i class="fa fa-sort-asc rotar color-azul" aria-hidden="true"></i> Estaciones utilizadas
             </h3>
           </div>
           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">        
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">
               <div class="puntoAzul"></div> <span>Estación disponible</span>
             </div>
-              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">        
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">
               <div class="puntoRojo"></div> <span>Cargando gasolina</span>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">        
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 form-group listas-fij">
               <div class="puntoVerde"></div> <span>Recarga recientes</span>
             </div>
-            </div>        
+            </div>
         </div>
 
       <!-- fin mapa -->
